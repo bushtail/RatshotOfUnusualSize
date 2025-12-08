cfa562c923c8bc9b3684d9fc44ff98e39c563855
bushtail <evannosich@icloud.com>
2025-12-07 23:44:13 -0500

    init, no bundles

---
+++ RatshotOfUnusualSize/Ammo_RatshotOfUnusualSize.cs
@@ -0,0 +1,53 @@
+﻿using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+
+namespace RatshotOfUnusualSize;
+
+public record Ammo_RatshotOfUnusualSize : NewItemFromCloneDetails
+{
+    public override MongoId? ItemTplToClone { get; set; } = ItemTpl.AMMO_127X99_M21;
+
+    public override TemplateItemProperties? OverrideProperties { get; set; } = new()
+    {
+        Prefab = new Prefab
+        {
+            Path = "ammo_50bmg_ratshot.bundle",
+            Rcid = ""
+        },
+        Damage = 0.5,
+        ProjectileCount = 1000,
+        BuckshotBullets = 1000,
+        Weight = 115,
+        AmmoAccr = 0,
+        AmmoType = "buckshot",
+        RicochetChance = 0,
+        AmmoTooltipClass = "SoftTargets",
+        ArmorDamage = 15,
+        BallisticCoeficient = 0.025,
+        BulletDiameterMillimeters = 0.0,
+        BulletMassGram = 0.00,
+        CanSellOnRagfair = false,
+        PenetrationPower = 1,
+        PenetrationPowerDeviation = 1,
+        StaminaBurnPerDamage = 0.1
+    };
+
+    public override string? ParentId { get; set; } = "5485a8684bdc2da71d8b4567";
+    public override string? NewId { get; set; } = "69363bc9d09da95973053053";
+    public override double? FleaPriceRoubles { get; set; } = 4200;
+    public override double? HandbookPriceRoubles { get; set; } = 690;
+    public override string? HandbookParentId { get; set; } = "5b47574386f77428ca22b33b";
+
+    public override Dictionary<string, LocaleDetails>? Locales { get; set; } = new()
+    {
+        {
+            "en", new LocaleDetails
+            {
+                Name = ".50 BMG Ratshot",
+                ShortName = "Ratshot",
+                Description = "A .50 BMG (12.7x99mm NATO) Ratshot cartridge, developed in Florin and adopted in [ERR_DATE_NULL] for the Barrett M107, primarily designed for the eradication of rodents of unusual size. The cartridge has gained a significant alternate use as an anti-personnel cartridge as its count of one thousand lead pellets deliver a devastating blow to multiple combatants if given enough time to expand."
+            }
+        }
+    };
+}
---
+++ RatshotOfUnusualSize/RatshotOfUnusualSize.cs
@@ -0,0 +1,101 @@
+﻿using JetBrains.Annotations;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Enums;
+using SPTarkov.Server.Core.Servers;
+using SPTarkov.Server.Core.Services.Mod;
+
+namespace RatshotOfUnusualSize;
+
+[UsedImplicitly, Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+public class RatshotOfUnusualSize(DatabaseServer databaseServer, CustomItemService customItemService) : IOnLoad
+{
+    private readonly NullReferenceException _nullReferenceException = new();
+    public Task OnLoad()
+    {
+        var itemsDb = databaseServer.GetTables().Templates.Items;
+        var traderDb = databaseServer.GetTables().Traders;
+        if (!traderDb.TryGetValue(Traders.REF, out var trader) || !itemsDb.TryGetValue(ItemTpl.MARKSMANRIFLE_THEAKGUY_AK50_50_BMG_SNIPER_RIFLE, out var weapon) || !itemsDb.TryGetValue(ItemTpl.MAGAZINE_127X99_M82_10RND, out var magazine)) throw _nullReferenceException;
+
+        var ammo = new Ammo_RatshotOfUnusualSize();
+        var ammoBox = new AmmoBox_RatshotOfUnusualSize_10RND();
+        
+        customItemService.CreateItemFromClone(ammo);
+        customItemService.CreateItemFromClone(ammoBox);
+        
+        if (!itemsDb.TryGetValue(new MongoId(ammo.NewId!), out var newBullet) || !itemsDb.TryGetValue(new MongoId(ammoBox.NewId!), out var newBulletBox)) throw _nullReferenceException;
+
+        var weaponProperties = weapon.Properties;
+        if (weaponProperties == null) throw _nullReferenceException;
+        
+        var chambers = weaponProperties.Chambers;
+        if (chambers == null) throw _nullReferenceException;
+        foreach (var chamber in chambers)
+        {
+            var chamberProps = chamber.Properties;
+            if (chamberProps == null) continue;
+            var filters = chamberProps.Filters;
+            if (filters == null) continue;
+            foreach (var filter in filters)
+            {
+                var _filter = filter.Filter;
+                _filter?.Add(newBullet.Id);
+            }
+        }
+        
+        var magazineProperties = magazine.Properties;
+        if (magazineProperties == null) throw _nullReferenceException;
+        
+        var cartridges = magazineProperties.Cartridges;
+        if (cartridges == null) throw _nullReferenceException;
+        foreach (var cartridge in cartridges)
+        {
+            var cartridgeProps = cartridge.Properties;
+            if (cartridgeProps == null) continue;
+            var filters = cartridgeProps.Filters;
+            if (filters == null) continue;
+            foreach (var filter in filters)
+            {
+                var _filter = filter.Filter;
+                _filter?.Add(newBullet.Id);
+            }
+        }
+
+        var assort = trader.Assort;
+        var assortItems = assort.Items;
+        var assortBarterSchemes = assort.BarterScheme;
+        var assortLoyalLevelItems = assort.LoyalLevelItems;
+        var newAssortID = new MongoId("693645d70a250a9e1a0e0db0");
+        
+        assortItems.Add(new Item
+        {
+            Id = newAssortID,
+            Template = newBulletBox.Id,
+            ParentId = "hideout",
+            SlotId = "hideout",
+            Upd = new Upd
+            {
+                UnlimitedCount = true,
+                StackObjectsCount = 9999999,
+                BuyRestrictionMax = 69,
+                BuyRestrictionCurrent = 0
+            }
+        });
+        
+        assortBarterSchemes.Add(newAssortID, [
+            [
+                new BarterScheme
+                {
+                    Count = 8,
+                    Template = ItemTpl.MONEY_GP_COIN
+                }
+            ]
+        ]);
+        
+        assortLoyalLevelItems.Add(newAssortID, 2);
+        
+        return Task.CompletedTask;
+    }
+}
